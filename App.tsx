c8416e19cfc8cefa389f0d033be638ef2c2cf603
phong3710 <nguyenthanhphong3710@gmail.com>
2024-03-12 17:33:23 +0700

    fix: add web view native component

--- App.tsx
+++ App.tsx
@@ -1,103 +1,28 @@
-import React, {useRef, useState} from 'react';
-import {
-  Button,
-  Dimensions,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-} from 'react-native';
-import {WebView, WebViewMessageEvent} from 'react-native-webview';
+import React from 'react';
+import {Dimensions, StyleSheet, View} from 'react-native';
+import {WebViewNative} from './component/WebViewNative';
 
 const DIMENSION = {
   width: Dimensions.get('screen').width,
-  height: Dimensions.get('screen').height / 2,
+  height: Dimensions.get('screen').height - 250,
 };
+
 function App(): React.JSX.Element {
-  const webViewRef = useRef<WebView>(null);
-  const [textString, setTextString] = useState('');
-
-  /**
-   *  Handle receive post message in native
-   * @param event
-   */
-  const onMessage = (event: WebViewMessageEvent) => {
-    const value = String(event.nativeEvent.data);
-    setTextString(value);
-  };
-
-  /**
-   * Function send post message original JS
-   */
-  const INJECTED_JAVASCRIPT = `(function() {
-    window.postMessage("${textString}", "*");
-  })();`;
-
-  /**
-   * Function receive post message in parent HTML and send post message
-   */
-  const INJECTED_JAVASCRIPT_RECEIVE = `(function() {
-    window.addEventListener('message', event => {
-      window.ReactNativeWebView.postMessage(event.data);
-    }, true);
-  })();`;
-
-  const onPress = () => {
-    webViewRef.current?.injectJavaScript(INJECTED_JAVASCRIPT);
-  };
-
+  const {width, height} = DIMENSION;
   return (
-    <SafeAreaView>
-      <Text style={styles.title}>Integration Webview</Text>
-      <View style={styles.webViewContainer}>
-        <WebView
-          scrollEnabled={false}
-          ref={webViewRef}
-          javaScriptEnabled
-          source={require('./assets/parent-index.html')}
-          onMessage={onMessage}
-          injectedJavaScriptBeforeContentLoaded={INJECTED_JAVASCRIPT_RECEIVE}
-        />
-      </View>
-      <View style={styles.nativeContainer}>
-        <Text style={styles.textNative}>
-          Enter the text to send post message
-        </Text>
-        <TextInput
-          style={styles.input}
-          value={textString}
-          onChangeText={setTextString}
-        />
-      </View>
-      <Button color={'red'} title="SUBMIT" onPress={onPress} />
-    </SafeAreaView>
+    <View style={styles.container}>
+      <WebViewNative
+        source={require('./assets/html/parent-index-1.html')}
+        width={width}
+        height={height}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  title: {
-    fontSize: 20,
-    textAlign: 'center',
-    marginTop: 20,
-  },
-  webViewContainer: {
-    width: DIMENSION.width,
-    height: DIMENSION.height,
-  },
-  nativeContainer: {
+  container: {
     gap: 10,
-  },
-  textNative: {
-    textAlign: 'center',
-  },
-  input: {
-    height: 30,
-    borderRadius: 10,
-    borderWidth: 1,
-    width: DIMENSION.width - 60,
-    alignSelf: 'center',
-    paddingHorizontal: 10,
   },
 });
 
---
+++ component/WebViewNative.tsx
@@ -0,0 +1,109 @@
+/* eslint-disable react/react-in-jsx-scope */
+import {useRef, useState} from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+  TextInput,
+  Button,
+  StyleSheet,
+  DimensionValue,
+} from 'react-native';
+import WebView, {WebViewMessageEvent} from 'react-native-webview';
+import {WebViewSource} from 'react-native-webview/lib/WebViewTypes';
+
+type Props = {
+  source: WebViewSource;
+  width?: DimensionValue;
+  height?: DimensionValue;
+};
+
+export const WebViewNative = ({
+  source,
+  width = '100%',
+  height = 200,
+}: Props) => {
+  const webViewRef = useRef<WebView>(null);
+  const [textString, setTextString] = useState('');
+
+  /**
+   *  Handle receive post message in native
+   * @param event
+   */
+  const onMessage = (event: WebViewMessageEvent) => {
+    const value = String(event.nativeEvent.data);
+    setTextString(value);
+  };
+
+  /**
+   * Function send post message original JS
+   */
+  const INJECTED_JAVASCRIPT = `(function() {
+    window.postMessage("${textString}", "*");
+  })();`;
+
+  /**
+   * Function receive post message in parent HTML and send post message
+   */
+  const INJECTED_JAVASCRIPT_RECEIVE = `(function() {
+    window.addEventListener('message', event => {
+      window.ReactNativeWebView.postMessage(event.data);
+    }, true);
+  })();`;
+
+  /**
+   * Run JS logic when press button
+   */
+  const onPress = () => {
+    webViewRef.current?.injectJavaScript(INJECTED_JAVASCRIPT);
+  };
+
+  return (
+    <SafeAreaView>
+      <Text style={styles.title}>Integration Webview</Text>
+      <View style={{width, height}}>
+        <WebView
+          scrollEnabled={false}
+          ref={webViewRef}
+          javaScriptEnabled
+          source={source}
+          onMessage={onMessage}
+          injectedJavaScriptBeforeContentLoaded={INJECTED_JAVASCRIPT_RECEIVE}
+        />
+      </View>
+      <View style={styles.nativeContainer}>
+        <Text style={styles.textNative}>
+          Enter the text to send post message
+        </Text>
+        <TextInput
+          style={[styles.input]}
+          value={textString}
+          onChangeText={setTextString}
+        />
+      </View>
+      <Button color={'red'} title="SUBMIT" onPress={onPress} />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 20,
+    textAlign: 'center',
+    marginTop: 20,
+  },
+  nativeContainer: {
+    gap: 10,
+  },
+  textNative: {
+    textAlign: 'center',
+  },
+  input: {
+    height: 30,
+    borderRadius: 10,
+    borderWidth: 1,
+    width: '50%',
+    alignSelf: 'center',
+    paddingHorizontal: 10,
+  },
+});
